e9f192f19470fe4f0de23920e84490edc9ee62c6
thfields <thigoabcfc@hotmal.com>
2023-08-27 22:32:50 -0300

    first commit

---
+++ backend/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
@@ -0,0 +1,42 @@
+package br.com.criandoapi.projeto.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import br.com.criandoapi.projeto.DAO.IUsuario;
+import br.com.criandoapi.projeto.model.Usuario;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private IUsuario dao;
+    @GetMapping
+    public List<Usuario> ListaUsuarios (){
+        return (List<Usuario>) dao.findAll();
+    }
+
+    @PostMapping
+    public Usuario criarUsuario(@RequestBody Usuario usuario){
+        Usuario usuarioNovo = dao.save(usuario);
+        return usuarioNovo;
+    }
+
+    @PutMapping
+    public Usuario editarUsuario(@RequestBody Usuario usuario){
+        Usuario usuarioNovo = dao.save(usuario);
+        return usuarioNovo;
+    }
+
+    @DeleteMapping("/{id}")
+    public Optional<Usuario> excluirUsuario(@PathVariable Integer id){
+        Optional<Usuario> usuario = dao.findById(id);
+        dao.deleteById(id);
+        return usuario;
+    }
+}
